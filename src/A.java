c3fd15871820f03c9d9484f4f2455f58f914ca4a
s54816 <eggah11@gmail.com>
2024-03-04 15:34:34 +0100

    h

--- src/A.java
+++ src/A.java
@@ -2,16 +2,8 @@ import java.util.Scanner;
 
 public class A {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        public static int sj(int x, int y, int z){
+            if
         }
     }
 }
