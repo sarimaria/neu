da2954dcf69b7d560d7067104953149984ad6b78
s54816 <eggah11@gmail.com>
2024-03-05 12:35:59 +0100

    h

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //System.out.println("Name:Sarah Egger");
+        //System.out.println("Wetter:Neblig");
+        //System.out.println("Katzenname:Mickey");
+
+        //String tag = "Donnerstag";
+        //String tag2 = "Freitag";
+        //String leerzeichen = " ";
+
+        //System.out.println("Heute ist ein schöner "+ tag);
+        //System.out.println(tag+"e sind meine Lieblingstage, da ich an einem " +tag+" immer motiviert bin");
+        //System.out.println(tag+leerzeichen+tag2);
+
+        //boolean b;
+        //int a = 115;
+        //b= a>= 110;
+        //System.out.println(b);
+
+        //Scanner blabla =  new Scanner(System.in);
+        //boolean bool;
+        //String vorname;
+        //String nachname;
+        //System.out.println("Geben Sie Ihren Vornamen ein: ");
+        //vorname = blabla.next();
+        //System.out.println("Die Eingabe: "+vorname);
+        //System.out.println("Geben Sie Ihren Nachnamen ein: ");
+        //nachname = blabla.next();
+        //String name = vorname+" "+nachname;
+        //System.out.println("name: "+name);
+        //bool = name.length()>10;
+        //int laenge = name.length();
+        //System.out.println("Die Länge ist größer 10: " +bool+ ",denn die exakte Länge ist: "+laenge);
+
+        //System.out.println("Willkommen "+name.toUpperCase());
+        //ausgabe();
+
+        // Scanner scanner = new Scanner(System.in);
+        //int nummer;
+        //System.out.println("Zahl einlesen: ");
+        //nummer = scanner.nextInt();
+        //if (nummer>5) {
+        //    System.out.println("Nummer ist größer 5");
+        //}
+        //else if (nummer<5) {
+        //    System.out.println("Nummer ist kleiner 5");
+        //}
+        //else {
+        //    System.out.println("Die Zahl ist gleich 5");
+        //}
+
+    }
+}
