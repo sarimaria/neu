8bb1fcad6e1ef7f81a2507647c6e3785cbe516ca
s54816 <eggah11@gmail.com>
2024-03-05 12:32:43 +0100

    h

--- src/A.java
+++ src/A.java
@@ -2,8 +2,24 @@ import java.util.Scanner;
 
 public class A {
     public static void main(String[] args) {
-        public static int sj(int x, int y, int z){
-            if
+        for (int i = 2025; i <= 2125 ; i++) {
+            System.out.println(sj);
         }
     }
-}
+        public static void sj(int i){
+        String schaltjahr;
+        String keinSJ;
+            if (i%4==0 && i%100==0){
+                return keinSJ;
+                }
+                else if (i%4==0 && i%400 ==0){
+                    return  schaltjahr;
+                }
+                else if(i%4==0){
+                return schaltjahr;
+            }
+                else{
+                System.out.println("Ubgültige Eingabe");
+            }
+            }
+    }
