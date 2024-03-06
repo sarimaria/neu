bba24de2b46afaab9e1a8978e3c5aa89d43d6f71
s54816 <eggah11@gmail.com>
2024-03-06 12:55:47 +0100

    Hausübung Bsp 2

--- src/Hausuebung.java
+++ src/Hausuebung.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
 public class Hausuebung {
-    public static void ungerade(int a) {
-        if (a % 2 == 0) { //Wenn a durch 2 ohne Rest teilbar ist, dann...
-            System.out.printf("Die Zahl " + a + " ist gerade");
-        } else {
-            System.out.printf("Die Zahl " + a + " ist ungerade");
-        }
-    }
     public static void main(String[] args) {
-            //Übung 1
-        Scanner scanner = new Scanner((System.in));
-        System.out.println("Bitte beliebige ganze Zahl eingeben: "); //Eingabe der Zahl durch User
-        int zahl = scanner.nextInt(); //Zahl einlesen und Wert in zahl speichern
-        ungerade(zahl); // Aufruf der Methode
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte Alter eingeben:");
+        int alter = scanner.nextInt();
+        if (alter >= 24) {
+            System.out.printf("Mit " + alter + " Jahren sind die Lenkerberechtigungen für AM, A1, A2, B/BE, C1/C1E und F möglich");
+        } else if (alter >= 21) {
+            System.out.println("Mit " + alter + " Jahren sind die Lenkerberechtigungen für C und D1/D1E möglich");
+        } else if (alter >= 18) {
+            System.out.printf("Mit " + alter + " Jahren sind die Lenkerberechtigungen für A2, B/BE, C1/C1E und F möglich");
+        } else if (alter >= 17) {
+            System.out.printf("Mit " + alter + " Jahren sind die Lenkerberechtigungen für B mit 17 möglich");
+        } else if (alter >= 16) {
+            System.out.printf("Mit " + alter + " Jahren sind die Lenkerberechtigungen für A1 und F möglich");
+        } else if (alter >= 15) {
+            System.out.printf("Mit " + alter + " Jahren ist die Lenkerberechtigungen für AM möglich");
+        } else {
+            System.out.printf("Zu Jung zum Fahren");
+        }
     }
 }
